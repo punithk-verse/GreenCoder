8c5bb50f5cefc5c592227842fa760b30eef45d1f
PUNITH K <punith34k@gmail.com>
2026-02-22 11:30:39 +0530

    Refactor analyze_code function for clarity
    indentation

--- analysis.py
+++ analysis.py
@@ -17,16 +17,16 @@ def analyze_code(code:str):
         if len(matches)>1:
             recursion=True
             break
-        workload=(total_complexity*1.5)+(total_loops*2)+(loc*0.05) 
-        energy_e=workload*0.01
-        carbon_e=energy_e*0.475
-        complexity_penalty=total_complexity *2
-        loop_penalty=total_loops *4
-        recursion_penalty=5 if recursion else 0
-        loc_penalty=loc *0.1
-        raw_score=100-(complexity_penalty+loop_penalty+recursion_penalty+loc_penalty)
-        efficiency_score=max(0,min(100,round(raw_score,2)))
-        return {
+    workload=(total_complexity*1.5)+(total_loops*2)+(loc*0.05) 
+    energy_e=workload*0.01
+    carbon_e=energy_e*0.475
+    complexity_penalty=total_complexity *2
+    loop_penalty=total_loops *4
+    recursion_penalty=5 if recursion else 0
+    loc_penalty=loc *0.1
+    raw_score=100-(complexity_penalty+loop_penalty+recursion_penalty+loc_penalty)
+    efficiency_score=max(0,min(100,round(raw_score,2)))
+    return {
             "complexity":total_complexity,
             "functions":num_functions,
             "lines of codes":loc,
@@ -37,3 +37,4 @@ def analyze_code(code:str):
             "efficiency score":efficiency_score
         }
     
+
